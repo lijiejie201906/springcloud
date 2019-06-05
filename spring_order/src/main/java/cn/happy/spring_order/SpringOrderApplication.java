fb0e4f5710b935d93cf3e818d75d51831bbd2972
lijiejie201906 <1392826262@qq.com>
2019-06-05 11:33:48 +0800

    Initial commit

---
+++ spring_order/src/main/java/cn/happy/spring_order/SpringOrderApplication.java
@@ -0,0 +1,13 @@
+package cn.happy.spring_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrderApplication.class, args);
+	}
+
+}
